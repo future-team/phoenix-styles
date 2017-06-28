9cf3999cc54e36a7267047bb26b65c1b0233b4df
chenaibing <348549708@qq.com>
2017-06-28 14:41:39 +0800

    button group fixed

--- gulpfile.js
+++ gulpfile.js
@@ -31,7 +31,7 @@ gulp.task('clean', function () {
         .pipe(clean()).on('error', error );
 });
 
-gulp.task('webpack', function (done) {
+gulp.task('webpack', ['clean'], function (done) {
     webpack(webpackConfig).run(function (err, stats) {
         if (err) throw new gutil.PluginError("webpack", err);
         gutil.log("[webpack]", stats.toString({}));
