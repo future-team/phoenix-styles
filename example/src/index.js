8463b762da385768366646e431a80b2281a33bb3
chenaibing <348549708@qq.com>
2017-07-25 14:31:25 +0800

    label

--- example/src/index.js
+++ example/src/index.js
@@ -8,13 +8,14 @@
 		var sections = $('.main section'),
 			navs = $('.main-nav li'),
 			scrollTop = parseInt($(document).scrollTop()),
-			thisTop, nextTop
-		
+			thisTop, nextTop,
+			clientHeight = document.documentElement.clientHeight
+
 		for(var i=0; i< sections.length; i++){
 			thisTop = sections.eq(i).offset().top
 			nextTop = sections.eq(i+1).offset()? sections.eq(i+1).offset().top : thisTop+sections.eq(i).offset().height
 
-			if(scrollTop >= thisTop - 250 && scrollTop < nextTop){
+			if(scrollTop >= thisTop - clientHeight+100 && scrollTop < nextTop){
 				var id = sections.eq(i).attr('id')
 				// if(!navs.eq(i).hasClass('active')) 
 					$('.main-nav li').removeClass('active')
